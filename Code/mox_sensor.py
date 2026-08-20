cc5da4b98bc9972bb5b367a97b7dcecbf675d97b
lou-schoolshls <lucan@shlutheran.org>
2026-08-20 13:34:24 -0500

    Added Basic Sensor Program

--- Code/mox_sensor.py
+++ Code/mox_sensor.py
@@ -1,1 +1,24 @@
+# I2C has to be active on Pi to work with this
 
+""" 
+Install Commands:
+pip3 install adafruit-blinka
+pip3 install adafruit-circuitpython-ens160
+"""
+
+# IMPORTS
+import time
+import board
+import adafruit_ens160
+
+# INIT I2C BUS
+i2c = board.I2C()
+ens = adafruit_ens160.ENS160(i2c)
+
+# PRINT SENSOR DATA
+while True:
+    print("AQI:", ens.aqi)
+    print("TVOC: {} ppb".format(ens.tvoc))
+    print("eCO2: {} ppm".format(ens.eco2))
+    print("-" * 20)
+    time.sleep(1)
